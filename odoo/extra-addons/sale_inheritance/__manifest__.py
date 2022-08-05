41b83701f316d8bfe2950d49ed04eb3a8af2711c
DESKTOP-QD7JEVF\ADMIN <linhnguyenyoyo99@gmail.com>
2022-08-05 08:51:40 +0700

    Linh - tạo sale inheritance

---
+++ odoo/extra-addons/sale_inheritance/__manifest__.py
@@ -0,0 +1,16 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Sales inheritance",
+    "version": "15.0",
+    # "author": "anhnth.mta@gmail.com",
+    "sequence": "8",
+    "license": "LGPL-3",
+    "category": "Hidden",
+    "depends": ["sale", 'coupon', "sale_coupon", "product"],
+    "data": [
+        'views/sale_view.xml',
+    ],
+    "installable": True,
+    # "maintainers": ["anhnth.mta@gmail.com"]
+}
